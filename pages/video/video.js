f5bb799f4a5b6f4daa4c033b08a3b7c78f985c9d
fengyu <fengyu@pharmaland.fr>
2017-01-19 17:01:41 +0100

    video

--- pages/video/video.js
+++ pages/video/video.js
@@ -1,18 +1,42 @@
 import videoAPI from '../../api/video.js'
+import WxParse from '../../wxParse/wxParse.js'
 
 //获取应用实例
 var app = getApp()
 Page({
   data: {
-    video: {}
-  },
-  
-  onLoad(options) {
-  	this.setData({
-  		video: videoAPI.loadVideoById(options.id)
-  	})
+    video: {},
+    navTab: ["字幕", "讲解", "评论"],
+    currentNavtab: "0",
+    isLoading: true,
   },
 
-  onReady: function() {
+  onLoad(options) {
+    const v = videoAPI.loadVideoById(options.id);
+    const frsArray = v.parsed_content.split('||');
+    const length = frsArray.length;
+    for (let i = 0; i < length; i++) {
+      WxParse.wxParse('fr' + i, 'html', frsArray[i], this);
+      if (i === length - 1) {
+        WxParse.wxParseTemArray("frs", 'fr', length, this)
+      }
+    }
+    // WxParse.wxParse('frs[' + key + ']', 'html', this.data.test, this);
+    this.setData({
+      video: v,
+    })
+  },
+
+  onReady() {
+  },
+
+  switchTab(e){
+    this.setData({
+      currentNavtab: e.currentTarget.dataset.idx
+    });
+  },
+
+  showDict(e) {
+   console.log('clicked', e.currentTarget);
   }
 })
